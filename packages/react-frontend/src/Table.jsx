60fad3501af1c6be4b5574658e9fdb5c4fa4e868
Andy Cai <andycaai00@gmail.com>
2026-08-28 13:30:21 -0700

    table func update

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -36,34 +36,12 @@ function TableBody() {
 }
 
 function Table() {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
-        </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Bouncer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-          <td>Bartender</td>
-        </tr>
-      </tbody>
-    </table>
-  );
+    return (
+      <table>
+        <TableHeader />
+        <TableBody />
+      </table>
+    );
 }
 
 export default Table;
