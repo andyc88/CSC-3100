09a461e001423695e2fb8f7c5a1f92cc37a096ef
Andy <andycaai00@gmail.com>
2026-09-07 21:33:42 -0700

    findUserID helper func + endpoint

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -43,6 +43,7 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
 
+//find UserByName helper function + endpoint
 const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
@@ -55,6 +56,20 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+//find userByID helper function + endpoint
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
