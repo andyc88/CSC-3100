334c2ae4b57b7bc3503194701582d2353314ed85
Andy <andycaai00@gmail.com>
2026-09-07 21:23:24 -0700

    Added specific name search endpoint

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,20 +3,6 @@ import express from "express";
 const app = express();
 const port = 8000;
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-})
-
-app.get("/users", (req, res) => {
-    res.send(users);
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-})
-
 const users = {
   users_list: [
     {
@@ -46,3 +32,29 @@ const users = {
     },
   ],
 };
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
+
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
