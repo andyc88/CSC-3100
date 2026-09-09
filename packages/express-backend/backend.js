e22c668d3d872a8bb4f1fb6b33eb1eb051b713ba
Andy Cai <andycaai00@gmail.com>
2026-09-08 21:09:48 -0700

    deleted redundent get/users/ endpoint

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -43,21 +43,10 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
 
-//find UserByName helper function + endpoint
+//find UserByName helper -> used in get/users endpoint
 const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
-
-app.get("/users", (req, res) => {
-  const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } else {
-    res.send(users);
-  }
-});
 
 //find userByID helper function + endpoint
 const findUserById = (id) =>
