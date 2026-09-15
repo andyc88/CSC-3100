19cc0c25b31f507dc8b06b7e8a1675e6fcb1f55a
Andy <andycaai00@gmail.com>
2026-09-15 14:22:56 -0700

    cors process line added

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -33,6 +34,8 @@ const users = {
   ],
 };
 
+//enable all CORS requests
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
